7211f8f56b58f0b599db1b22f4e969d7b345b410
Abhishek Khati <abhishekkhati39@gmail.com>
2025-01-25 07:02:43 +0545

    new changes

--- src/mathutils.js
+++ src/mathutils.js
@@ -8,4 +8,28 @@ export const factorial = (n) => {
   return factorial;
 };
 
-export const gcd = (a, b) => {};
+export const findGCD = (a, b) => {
+  a = Math.abs(a);
+  b = Math.abs(b);
+
+  while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
+  }
+
+  return a;
+};
+
+export const fibonacciSeries = (n) => {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const series = [0, 1];
+  for (let i = 2; i < n; i++) {
+    series.push(series[i - 1] + series[i - 2]);
+  }
+
+  return series;
+};
---
+++ src/string.js
@@ -0,0 +1,7 @@
+export const toUppercase = (str) => {
+  return str.toUpperCase();
+};
+
+export const reverse = (str) => {
+  return str.split("").reverse().join("");
+};
